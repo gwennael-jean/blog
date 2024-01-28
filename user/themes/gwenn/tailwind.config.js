340c9a2f9c4bf7f15d101d38b34935dc6abc6990
Gwennael Jean <gwennael.jean@linkt.fr>
2024-01-28 11:25:50 +0100

    feat(assets): configuration de tailwindcss

---
+++ user/themes/gwenn/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./templates/**/*.twig",
+        "./assets/css/**/*.css",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
